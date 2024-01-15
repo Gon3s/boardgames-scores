7e591588c838413a7b09ce6fdd7fb4a689ee2afd
Kevin Cuoq <kevin.cuoq@apside-groupe.com>
2024-01-15 16:31:44 +0100

    Remove firebase

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 
 ## Getting Started
 
+### Clone project
+
+Rename `flutter_starter_kit` :
+- `pubspec.yaml`: name and description
+- `android\app\build.gradle`: namespace & applicationId
+- `android\app\src\main\kotlin\com\example\flutter_starter_kit\MainActivity.kt`: package
+- `android\app\src\main\AndroidManifest.xml`: android:labels
+- `ios\Runner\Info.plist`: CFBundleName
+
 ## Dependancies
 
 - Riverpod
@@ -18,3 +27,4 @@ All translate are in `l10n/` one file by locale
 For add a new locale :
 - Use `Flutter Intl: Add locale`
 - Add it in `ios/Runner/Info.plist`
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/app.dart';
 import 'core/app_env.dart';
-import 'firebase_options.dart';
 
 Future<void> mainCommon(Environment env) async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await _initFirebase();
 
   _setupSystemUIOverlayStyle();
   _setupPreferredOrientations();
@@ -19,12 +15,6 @@ Future<void> mainCommon(Environment env) async {
     const ProviderScope(
       child: MyApp(),
     ),
-  );
-}
-
-Future<void> _initFirebase() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
 

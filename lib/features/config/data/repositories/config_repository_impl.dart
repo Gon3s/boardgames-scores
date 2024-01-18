d2b5540bae6ebdf2f740dd9ce0dfb9e56d9ae7a0
Kevin Cuoq <kevin.cuoq@apside-groupe.com>
2024-01-18 09:34:02 +0100

    Fix problems

--- lib/features/config/data/repositories/config_repository_impl.dart
+++ lib/features/config/data/repositories/config_repository_impl.dart
@@ -1,8 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../../core/error/exception.dart';
-import '../../../../core/error/failure.dart';
-import '../../../../core/utils/colored_debug_printer.dart';
+import '../../../../core/error/index.dart';
 import '../../domain/entities/config.dart';
 import '../../domain/repositories/config_repository.dart';
 import '../data_sources/local_data_source.dart';
